3a0e855de0b5c5a4a9b7096a355d783cffd03814
Graham Moore <gramoore@outlook.com>
2023-10-21 11:58:12 +0200

    update flags

--- cmd/schema/main.go
+++ cmd/schema/main.go
@@ -6,25 +6,8 @@ import (
 )
 
 func main() {
-	// Define command-line flags
-	var (
-		intFlag   int
-		strFlag   string
-		boolFlag  bool
-		floatFlag float64
-	)
-
-	flag.IntVar(&intFlag, "int", 0, "An integer flag")
-	flag.StringVar(&strFlag, "str", "", "A string flag")
-	flag.BoolVar(&boolFlag, "bool", false, "A boolean flag")
-	flag.Float64Var(&floatFlag, "float", 0.0, "A float64 flag")
-
-	// Parse the command-line arguments
+	var closedWorldFlag bool
+	flag.BoolVar(&closedWorldFlag, "closedWorld", false, "Closed world assumption. Only allow what is defined in the model.")
 	flag.Parse()
-
-	// Access the values of the parsed flags
-	fmt.Printf("intFlag: %d\n", intFlag)
-	fmt.Printf("strFlag: %s\n", strFlag)
-	fmt.Printf("boolFlag: %v\n", boolFlag)
-	fmt.Printf("floatFlag: %f\n", floatFlag)
+	fmt.Printf("closedWorldFlag: %v", closedWorldFlag)
 }
